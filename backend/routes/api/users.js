aebbda3219681a02fcea7e17c280ee4d57e75aac
Ammanue <ammanuelgizawcoding@gmail.comm>
2022-07-29 18:42:07 -0400

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -28,9 +28,11 @@ const validateSignup = [
     .withMessage('Password must be 6 characters or more.'),
   handleValidationErrors
 ];
-
 // Sign up
-router.post( '/', validateSignup,
+// Sign up
+router.post(
+  '/',
+  validateSignup,
   async (req, res) => {
     const { email, password, username } = req.body;
     const user = await User.signup({ email, username, password });
@@ -47,14 +49,14 @@ module.exports = router;
 
 
 // fetch('/api/users', {
-//     method: 'POST',
-//     headers: {
-//       "Content-Type": "application/json",
-//       "XSRF-TOKEN": `UxHPMnO7-SAJKTkkGg27Efo6AoFm7N43tGfc`
-//     },
-//     body: JSON.stringify({
-//       email: 'spidey@spider.man',
-//       username: 'Spidey',
-//       password: 'password'
-//     })
-//   }).then(res => res.json()).then(data => console.log(data));
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": `FvWwxsxZ-ym7HVcjRf269DO64YX6qnKGPb88`
+//   },
+//   body: JSON.stringify({
+//     email: 'firestar@spider.man',
+//     username: 'Firestar',
+//     password: ''
+//   })
+// }).then(res => res.json()).then(data => console.log(data));
