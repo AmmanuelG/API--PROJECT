aff421364471ba324e3f706ea7e2d01f08974c41
Ammanue <ammanuelgizawcoding@gmail.comm>
2022-08-03 13:51:40 -0400

    seed file added

--- backend/db/migrations/20220803165246-create-spots.js
+++ backend/db/migrations/20220803165246-create-spots.js
@@ -1,27 +1,75 @@
 'use strict';
+
+const { sequelize } = require("../models");
+
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize)
+  {
     await queryInterface.createTable('Spots', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      }, 
+      name: { 
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true
       },
-      name: {
-        type: Sequelize.STRING
+      address: {
+        allowNull: false,
+        type: Sequelize.VARCHAR(30),
+        unique: true
+
       },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING(30)
+      },
+      state: {
+        allowNull: false,
+        type: Sequelize.STRING(30)
+      },
+      country: {
+        allowNull: false,
+        type: Sequelize.STRING(30)
+      },
+      lat: {
+        allowNull: false,
+        type:sequelize.DECIMAL,
+        unique: true  
+      },
+
+      lng:{
+        allowNull: false,
+        type:sequelize.DECIMAL,
+        unique: true
+      },
+      description:{
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+
+      price:{
+        allowNull: false,
+        type: Sequelize.INTEGER 
+      },
+
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     });
   },
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize)
+  {
     await queryInterface.dropTable('Spots');
   }
 };
--- backend/db/models/spots.js
+++ backend/db/models/spots.js
@@ -14,8 +14,63 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Spots.init({
-    name: DataTypes.STRING
-  }, {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.VARCHAR(30),
+      allowNull: false,
+      validate: {
+        len: [3, 256],
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      allowNull: false,
+      type: Sequelize.STRING(30)
+    },
+    country: {
+      allowNull: false,
+      type: Sequelize.STRING(30)
+    },
+    lat: {
+      allowNull: false,
+      type:sequelize.DECIMAL,
+      unique: true  
+    },
+    lng:{
+      allowNull: false,
+      type:sequelize.DECIMAL,
+      unique: true
+    },
+    description:{
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    price:{
+      allowNull: false,
+      type: Sequelize.INTEGER 
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    }
+  }, 
+  
+  
+  
+  
+  {
     sequelize,
     modelName: 'Spots',
   });
